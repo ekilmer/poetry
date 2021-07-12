9d021737bcd0b77e759d24534eb04ac851a84903
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-07-12 11:01:46 -0400

    Update

--- poetry/utils/pip.py
+++ poetry/utils/pip.py
@@ -60,7 +60,7 @@ def pip_install(
         raise PoetryException(f"Failed to install {path.as_posix()}") from e
 
 
-def pip_editable_install(directory: Path, environment: Env) -> Union[int, str]:
+def pip_editable_install(directory: Path, environment: Env, upgrade=True) -> Union[int, str]:
     return pip_install(
         path=directory, environment=environment, editable=True, deps=False, upgrade=True
     )
